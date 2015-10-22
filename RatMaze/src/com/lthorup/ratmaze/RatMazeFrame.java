478dcc5e458ea906a4f9fcc52283cee8c9cb36d6
Layne Thorup <layne@lthorup.com>
2015-10-22 17:12:49 -0600

    Resized window.

--- RatMaze/src/com/lthorup/ratmaze/RatMazeFrame.java
+++ RatMaze/src/com/lthorup/ratmaze/RatMazeFrame.java
@@ -33,7 +33,7 @@ public class RatMazeFrame extends JFrame {
 	public RatMazeFrame() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 432, 452);
+		setBounds(100, 100, 432, 455);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
